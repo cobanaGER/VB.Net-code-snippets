428f8558908223936ca7ff5680a59db47eeee4b0
cobanaGER <petri1234@aol.com>
2017-06-26 12:09:28 +0200

    Add files via upload

---
+++ Word01/Word01/Form1.vb
@@ -0,0 +1,106 @@
+﻿Imports Microsoft.Office.Interop.Word
+Imports Microsoft.Office.Interop
+''' <summary>
+''' Test zum Erstellen eines WORD-Dokuments
+''' <param name="sender">Typ Object</param> 
+''' <param name="e">as EventArgs</param> 
+''' </summary>
+''' <remarks></remarks>
+Public Class Form1
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        fWord()
+    End Sub
+    ''' <summary>
+    ''' Die Funktion wird vom Form1 mit Button1 gestartet
+    ''' </summary>
+    ''' <returns>true oder false</returns>
+    ''' <remarks></remarks>
+    Public Function fWord() As Boolean
+        Dim WordApp As Word.Application
+        Dim WordDatei As Word.Document
+        Try
+            WordApp = New Word.Application()
+            WordDatei = WordApp.Documents.Open("C:\tmp\testdok2.doc")
+            WordDatei.MailMerge.MainDocumentType = Word.WdMailMergeMainDocType.wdFormLetters
+            WordDatei.MailMerge.OpenDataSource(Name:= _
+                "C:\tmp\testcsv.csv", _
+                ConfirmConversions:=False, ReadOnly:=False, LinkToSource:=True, _
+                AddToRecentFiles:=False, PasswordDocument:="", PasswordTemplate:="", _
+                WritePasswordDocument:="", WritePasswordTemplate:="", Revert:=False, _
+                Format:=Word.WdOpenFormat.wdOpenFormatAuto, Connection:="", SQLStatement:="", SQLStatement1 _
+                :="", SubType:=Word.WdMergeSubType.wdMergeSubTypeOther)
+            With WordDatei.MailMerge
+                '.Destination = Word.WdMailMergeDestination.wdSendToNewDocument
+                .Destination = Word.WdMailMergeDestination.wdSendToEmail
+                .MailFormat = Word.WdMailMergeMailFormat.wdMailFormatHTML
+                .MailSubject = "Testmail "
+                .MailAddressFieldName = "email"
+                .SuppressBlankLines = True
+                With .DataSource
+                    .FirstRecord = Word.WdMailMergeDefaultRecord.wdDefaultFirstRecord
+                    .LastRecord = Word.WdMailMergeDefaultRecord.wdDefaultLastRecord
+                End With
+                .Execute(Pause:=False)
+            End With
+
+            WordDatei.Close()
+            WordApp.Visible = True
+            WordApp.Quit()
+
+            Return True
+        Catch ex As Exception
+            MsgBox(ex.ToString)
+            Return False
+        End Try
+    End Function
+    '/////////////////////////////////////////////////////////////////////////////////////////////
+    ' Dokument drucken, muss noch angepasst werden
+    Public Function pWord() As Boolean
+        Dim WordApp As Word.Application
+        Dim WordDatei As Word.Document
+        Try
+            WordApp = New Word.Application()
+            WordDatei = WordApp.Documents.Open("C:\tmp\testdok.doc")
+            WordDatei.MailMerge.MainDocumentType = Word.WdMailMergeMainDocType.wdFormLetters
+            WordDatei.MailMerge.OpenDataSource(Name:= _
+                "C:\tmp\testcsv.csv", _
+                ConfirmConversions:=False, ReadOnly:=False, LinkToSource:=True, _
+                AddToRecentFiles:=False, PasswordDocument:="", PasswordTemplate:="", _
+                WritePasswordDocument:="", WritePasswordTemplate:="", Revert:=False, _
+                Format:=Word.WdOpenFormat.wdOpenFormatAuto, Connection:="", SQLStatement:="", SQLStatement1 _
+                :="", SubType:=Word.WdMergeSubType.wdMergeSubTypeOther)
+            With WordDatei.MailMerge
+                '.Destination = Word.WdMailMergeDestination.wdSendToNewDocument
+                .Destination = Word.WdMailMergeDestination.wdSendToEmail
+                .MailFormat = Word.WdMailMergeMailFormat.wdMailFormatHTML
+                .MailSubject = "Testmail "
+                .MailAddressFieldName = "empfänger"
+                .SuppressBlankLines = True
+                With .DataSource
+                    .FirstRecord = Word.WdMailMergeDefaultRecord.wdDefaultFirstRecord
+                    .LastRecord = Word.WdMailMergeDefaultRecord.wdDefaultLastRecord
+                End With
+                .Execute(Pause:=False)
+            End With
+
+            WordDatei.Close()
+            WordApp.Visible = True
+            WordApp.Quit()
+
+            Return True
+        Catch ex As Exception
+            Return False
+        End Try
+    End Function
+    '/////////////////////////////////////////////////////////////////////////////////////////////
+    ''' <summary>
+    ''' Beendet das Programm
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    ''' <remarks></remarks>
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Me.Close()
+    End Sub
+End Class
---
+++ Word01/Word01/NeueKlasse.vb
@@ -0,0 +1,54 @@
+﻿Public Class NeueKlasse
+    ''' <summary>
+    ''' <seealso cref="TestFunction"/>
+    ''' </summary>
+    ''' <remarks></remarks>
+    Private Sub TestSub()
+
+    End Sub
+    ''' <summary>
+    ''' 
+    ''' </summary>
+    ''' <param name="sDatum"></param>
+    ''' <para>use <see cref="TestSub"/></para>
+    ''' <returns></returns>
+    ''' <remarks></remarks>
+    Private Function TestFunction(ByRef sDatum As Date) As String
+        Dim sname As String = Nothing
+        Try
+            Return sname
+        Catch ex As Exception
+            Return Nothing
+        End Try
+    End Function
+    ''' <summary>
+    ''' <para></para>
+    ''' <seealso> cref=" TestFunction"/></seealso>
+    ''' </summary>
+    ''' <remarks></remarks>
+    Private Sub xTest()
+
+    End Sub
+    ''' <param name="id">The ID of the record to update.</param>
+    ''' <remarks>Updates the record <paramref name="id"/>.
+    ''' <para>Use <see cref="DoesRecordExist"/> to verify that
+    ''' the record exists before calling this method.</para>
+    ''' </remarks>
+    Public Sub UpdateRecord(ByVal id As Integer)
+        ' Code goes here.
+    End Sub
+    ''' <param name="id">The ID of the record to check.</param>
+    ''' <returns><c>True</c> if <paramref name="id"/> exists,
+    ''' <c>False</c> otherwise.</returns>
+    ''' <remarks><seealso cref="UpdateRecord"/></remarks>
+    Public Function DoesRecordExist(ByVal id As Integer) As Boolean
+        ' Code goes here.
+    End Function
+    ''' <summary>
+    '''  Gets the collection of source XML files.  See <see cref="DataTable"/>
+    ''' </summary>
+    ''' <remarks></remarks>
+    Public Sub ttest()
+
+    End Sub
+End Class
